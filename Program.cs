0d043b2bc7ff99c616fea4f42cf695e363b31e5d
Jack-Peralta-Student <japecc2@student.oaisd.org>
2024-10-14 14:28:41 +0000

    Adding Console.WriteLine examples.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+        Console.WriteLine("My name is Jack Peralta");
+
+        //Using Variables
+        int age = 16;
+        string greeting = "hello world";
+        string firstname = "Jack";
+        string lastname = "Peralta";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " "
+        + lastname + ". I am " + age + " years old. I just spent $" + money + " on a coffee shop.");
+    }
+}
